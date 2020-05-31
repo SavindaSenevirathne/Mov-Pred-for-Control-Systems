0c2986a2be0c50b414497bc82190f4af4f7931e9
unknown <gayantrr@gmail.com>
2020-05-31 09:52:20 +0530

    Added automaticlly train part to UI and did some UI button changes like enabled and active

--- GUI.py
+++ GUI.py
@@ -17,6 +17,8 @@ import imutils
 import os
 from Pose_Detection import PoseDetection
 from opencvFaceRecognition.recognize_person import detectPerson
+from opencvFaceRecognition.extact_embeddings_person import extractEmbeddings
+from opencvFaceRecognition.train_model_person import trainModel
 
 
 
@@ -37,21 +39,23 @@ class Interface:
 
 		# create a button, that when pressed, will take the current
 		# frame and save it to file
-		btn = tki.Button(self.root, text="Capture",
-			command=self.takeSnapshot)
-		btn.pack(side="bottom", expand="no", padx=10,
-			pady=10)
-		btn = tki.Button(self.root, text="Start", command=self.startSkeletonDetection)
-		btn.pack(side="bottom", expand="no", padx=10,
-			pady=10)
-		btn = tki.Button(self.root, text="Confirm", command=self.confirmRecognition)
-		btn.pack(side="bottom", expand="no", padx=10,
-			pady=10)
+		self.btn1 = tki.Button(self.root, text="Capture",
+							   command=self.takeSnapshot, state="disabled")
+		self.btn1.pack(side="bottom", expand="no", padx=10,
+					   pady=10)
+		self.btn2 = tki.Button(
+			self.root, text="Start", command=self.startSkeletonDetection, state="disabled")
+		self.btn2.pack(side="bottom", expand="no", padx=10,
+					   pady=10)
+		self.btn3 = tki.Button(self.root, text="Confirm",
+							   command=self.confirmRecognition)
+		self.btn3.pack(side="bottom", expand="no", padx=10,
+					   pady=10)
 		self.personNameVar = tki.StringVar()
 		self.personNameVar.set('Recognized Person: ')
 		nameLabel = tki.Label(self.root, textvariable=self.personNameVar)
 		nameLabel.pack(side="bottom", expand="no", padx=10,
-			pady=10)
+					   pady=10)
 
 		# start a thread that constantly pools the video sensor for
 		# the most recently read frame
@@ -62,6 +66,7 @@ class Interface:
 		# set a callback to handle when the window is closed
 		self.root.wm_title("Movement Prediction")
 		self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+		self.performFaceRecognition = True
 		self.performSkeletonDetection = False
 
 	def videoLoop(self):
@@ -78,7 +83,7 @@ class Interface:
 				_, self.frame = self.vs.read()
 				# self.frame = imutils.resize(self.frame, width=600)
 				# self.frame = cv2.resize(
-                #     self.frame, int(600), interpolation=cv2.INTER_AREA)
+				#     self.frame, int(600), interpolation=cv2.INTER_AREA)
 				if self.frame is None:
 					print('--(!) No captured frame -- Break!')
 					break
@@ -86,15 +91,17 @@ class Interface:
 				h, w, _ = self.frame.shape
 				if((h > 700) | (w > 1366)):
 					self.frame = cv2.resize(
-                    	self.frame, (int(w/2), int(h/2)), interpolation=cv2.INTER_AREA)
+						self.frame, (int(w/2), int(h/2)), interpolation=cv2.INTER_AREA)
 
-				if (not self.performSkeletonDetection):
+				if (self.performFaceRecognition):
 					self.frame, self.detectedPersonName = detectPerson().detect_face(self.frame)
-					self.personNameVar.set('Recognized Person: ' + self.detectedPersonName)
+					self.personNameVar.set(
+						'Recognized Person: ' + self.detectedPersonName)
 				# perform skeleton detection on button press
 				if (self.performSkeletonDetection):
 					if currentNumber % 30 == 0:
-						self.frame, frameCopy = PoseDetection.detectPose(self.frame, False)
+						self.frame, frameCopy = PoseDetection.detectPose(
+							self.frame, False)
 					currentNumber += 1
 		
 				# OpenCV represents images in BGR order; however PIL
@@ -103,13 +110,13 @@ class Interface:
 				image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
 				image = Image.fromarray(image)
 				image = ImageTk.PhotoImage(image)
-		
+  
 				# if the panel is not None, we need to initialize it
 				if self.panel is None:
 					self.panel = tki.Label(image=image)
 					self.panel.image = image
 					self.panel.pack(side="left", padx=10, pady=10)
-		
+
 				# otherwise, simply update the panel
 				else:
 					self.panel.configure(image=image)
@@ -119,23 +126,40 @@ class Interface:
 			print("[INFO] caught a RuntimeError")
 
 	def takeSnapshot(self):
-		# grab the current timestamp and use it to construct the
-		# output path
-		ts = datetime.datetime.now()
-		filename = "{}.jpg".format(ts.strftime("%Y-%m-%d_%H-%M-%S"))
-		p = os.path.sep.join((self.outputPath, filename))
-
-		# save the file
-		cv2.imwrite(p, self.frame.copy())
-		print("[INFO] saved {}".format(filename))
+		ret, frame = self.vs.read()
+		img_name = "{}.png".format(self.img_counter)
+		cv2.imwrite(os.path.join(self.path, img_name), frame)
+		self.img_counter += 1
+		if(self.img_counter > 5):
+			self.btn2["state"] = "active"
 
 	def startSkeletonDetection(self):
+		if(self.detectedPersonName == "unknown"):
+			self.detectedPersonName = self.directory
+			extractEmbeddings().embedding()
+			trainModel().training()
+		self.btn1["state"] = "disabled"
+		self.btn2["state"] = "disabled"
 		self.vs.release()
 		self.vs = cv2.VideoCapture('media/common2.MOV')
-		self.performSkeletonDetection = not self.performSkeletonDetection
+		self.performFaceRecognition = False
+		self.performSkeletonDetection = True
 
 	def confirmRecognition(self):
-		print(self.detectedPersonName)
+		self.performFaceRecognition = False
+		if(self.detectedPersonName == "unknown"):
+			self.img_counte = 0
+			self.btn3["state"] = "disabled"
+			self.btn1["state"] = "active"
+			todayTime = datetime.datetime.now().strftime("%Y%b%d_%H%M%S")
+			self.img_counter = 0
+			parent_dir = "opencvFaceRecognition/dataset/"
+			self.directory = todayTime
+			self.path = os.path.join(parent_dir, self.directory)
+			os.mkdir(self.path)
+		else:
+			self.btn3["state"] = "disabled"
+			self.btn2["state"] = "active"
 
 	def onClose(self):
 		# set the stop event, cleanup the camera, and allow the rest of
@@ -148,9 +172,9 @@ class Interface:
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-o", "--output", default="output",
-	help="path to output directory to store snapshots")
+				help="path to output directory to store snapshots")
 ap.add_argument("-p", "--picamera", type=int, default=0,
-	help="whether or not the Raspberry Pi camera should be used")
+				help="whether or not the Raspberry Pi camera should be used")
 args = vars(ap.parse_args())
 
 # initialize the video stream and allow the camera sensor to warmup
